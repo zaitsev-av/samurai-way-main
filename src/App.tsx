32767e2ee86398ca8b39e04998e07ef76a7ef0a9
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-01-11 23:14:43 +0300

    Created component Accordion.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+function App(props: any) {
+    console.log('App rendering')
+    return (
+        <div>
+            <PageTitle title ={'This is APP component'}/>
+            <PageTitle title ={'User'}/>
+            Article 1
+            <Rating value ={3}/>
+            <Accordion titleValue={'Menu'} collapsedMenu = {false}/>{/*// Передаём title в AccordionTitle, пока туплю здесь но вроде разобрался.
+            //По факут текст 'Article One' передаётся в AccordionTitle но сейчас но как бы лежит в Accordion,
+            //для того чтобы предать его в AccordionTitle нам нужно в компаненте Accordion написать props, далее
+            передать полученый ею props
+            //в AccordionTitle. И уже в AccordionTitle в части где передается jsx разметка необходимо написать
+            пропс.title*/}
+            <Accordion titleValue={'Users'} collapsedMenu = {false}/> {/*Делаем так чтобы меню сворачивалось.
+            Схема передачи пропсов следующая. в Основной компонент Accordion передается пропс  collapsedMenu с значением true или false
+            далее типизируем получаемые значения в AccordionPropsType. Далее в самой компаненте Accordion в теге JSX AccordionBody передаем
+            это значение  самому компоненту AccordionBody. Далее типизируем этот компонент и прописываем разметку JSX через if else.*/}
+            Article 2
+            <Rating value ={0}/>
+            <Rating value ={props.rating}/>
+            <Rating value ={1}/>
+            <Rating value ={2}/>
+            <Rating value ={3}/>
+            <Rating value ={4}/>
+            <Rating value ={5}/>
+        </div>
+    )
 }
+
 
 export default App;
